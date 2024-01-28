579e9b40dc19d39fb59354ceb946c4329a9c3593
OrvokkiK <orvokki.kettula@metropolia.fi>
2024-01-28 19:53:08 +0200

    Added correct format for details, added decor in css

--- FEscripts.js
+++ FEscripts.js
@@ -1,10 +1,18 @@
 //Hakee selaimen kielen, koon ja nykyisen päivän ja ajan
-let language = window.navigator.language;
-let height = window.innerHeight;
-let width = window.innerWidth;
-let date = new Date();
+const language = window.navigator.language;
+const height = window.innerHeight;
+const width = window.innerWidth;
+const screenWidth = screen.availWidth;
+const screenHeight = screen.availHeight;
+const date = new Date();
+const options =  {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+};
 
 //näyttää tiedot sivulla
 document.getElementById("browserLang").innerHTML = "Selaimen kieli: " + language;
-document.getElementById("browserSize").innerHTML = "Selaimen koko: " + width + " px " + height + " px";
-document.getElementById("browserTimeAndDate").innerHTML = "Saavuttu sisustolle: " + date.toLocaleDateString('fi-FI') + " klo " + date.toLocaleTimeString('fi-FI');
+document.getElementById("browserSize").innerHTML = "Selaimen koko: " + width + " px  X " + height + " px";
+document.getElementById("screenSize").innerHTML = "Näytön koko: " + screenWidth + " px  X " + screenHeight + " px";
+document.getElementById("browserTimeAndDate").innerHTML = "Saavuttu sisustolle: " + date.toLocaleDateString('fi-FI', options) + " klo " + date.toLocaleTimeString('fi-FI');
